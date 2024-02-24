f0a4c1048dfc083777602db69ac61e3e99fd76fb
Rakesh <sbrakeshrath@gmail.com>
2024-02-24 12:57:09 +0530

    Added base for github

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/geeksofgurukul-internship-assignment-one/frontend/dist/",
 })
